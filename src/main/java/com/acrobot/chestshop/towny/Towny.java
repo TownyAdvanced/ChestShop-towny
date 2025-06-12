2d8b87aa6f849e0f91ced726f55a550d0aaed473
FlagCourier <flagcourier@pm.me>
2025-06-12 07:59:28 -0500

    Merge pull request #4 from TheFlagCourier/master
    Cleanup Some Stuff, Increment Version (+0.0.1)

--- src/main/java/com/acrobot/chestshop/towny/Towny.java
+++ src/main/java/com/acrobot/chestshop/towny/Towny.java
@@ -2,43 +2,79 @@ package com.acrobot.chestshop.towny;
 
 import com.Acrobot.Breeze.Configuration.Configuration;
 import com.acrobot.chestshop.towny.properties.Properties;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
+
+import static java.util.logging.Level.SEVERE;
 
 /**
  * @author Acrobot
  */
 public class Towny extends JavaPlugin {
-    private static File dataFolder;
+    private File dataFolder;
 
+    @Override
     public void onEnable() {
         dataFolder = getDataFolder();
 
-        Configuration.pairFileAndClass(loadFile("config.yml"), Properties.class);
+        Optional<File> config = loadFile("config.yml");
+        if (config.isPresent()) {
+            Configuration.pairFileAndClass(config.get(), Properties.class);
+        } else {
+            getLogger().log(SEVERE, "Config File Not Present or Loaded. Disabling.");
+            onDisable();
+            return;
+        }
 
         if (Properties.BUILDING_INSIDE_PLOTS) {
             getServer().getPluginManager().registerEvents(new PlotListener(), this);
         }
     }
 
-    public static File loadFile(String string) {
-        File file = new File(dataFolder, string);
+    @Override
+    public void onDisable() {
+        dataFolder = null;
+        HandlerList.unregisterAll(this);
+    }
 
+    /**
+     * Ensures that a {@link File} exists, and tries to create the file if it does not.
+     * <br> This method assumes that the File is a child of the {@link #dataFolder}.
+     * @param string String representing a {@link File}.
+     * @return An {@link Optional}&lt;{@link File}&gt; for the supplied String.
+     */
+    public Optional<File> loadFile(String string) {
+        File file = new File(dataFolder, string);
         return loadFile(file);
     }
 
-    private static File loadFile(File file) {
+    private static Optional<File> loadFile(File file) {
         if (!file.exists()) {
             try {
-                file.getParentFile().mkdirs();
-                file.createNewFile();
+                if (!file.getParentFile().exists()) {
+                    boolean parentCreated = file.getParentFile().mkdirs();
+                    if (!parentCreated) {
+                        // Alternatively, could throw NoSuchFileException.
+                        throw new IOException(file.getParent());
+                    }
+                }
+                return createFile(file);
             } catch (IOException e) {
-                e.printStackTrace();
+                return Optional.empty();
             }
         }
+        return Optional.of(file);
+    }
 
-        return file;
+    private static Optional<File> createFile(File file) throws IOException {
+        boolean fileCreated = file.createNewFile();
+        if (fileCreated) {
+            return Optional.of(file);
+        }
+        return Optional.empty();
     }
 }
