ae26d2e0f49b43b953626f4046aff12f7068613c
Sulky_Whale <171066059+SulkyWhale@users.noreply.github.com>
2026-02-24 17:00:16 +0200

    Add separate buy and sell permissions for town and nation shops (#6)
    * Add separate buy and sell permissions for town and nation shops
    
    * Add proper exception handling
    
    * Use normal Exceptions

--- src/main/java/com/acrobot/chestshop/towny/TownyUtils.java
+++ src/main/java/com/acrobot/chestshop/towny/TownyUtils.java
@@ -1,5 +1,6 @@
 package com.acrobot.chestshop.towny;
 
+import com.Acrobot.Breeze.Utils.PriceUtil;
 import com.Acrobot.ChestShop.Database.Account;
 import com.acrobot.chestshop.towny.properties.Properties;
 import com.palmergames.bukkit.towny.TownyAPI;
@@ -194,5 +195,21 @@ public class TownyUtils {
             }
         }
         return "";
+    }
+
+    public static void checkShopPerms(Player player, String ownerLine, String priceLine) throws Exception {
+        if (TownyUtils.isTownShop(ownerLine)) {
+            if (PriceUtil.hasBuyPrice(priceLine) && !Permission.has(player, Permission.TOWN_SHOP_BUY)) {
+                throw new Exception("&cYou do not have permission.");
+            } else if (PriceUtil.hasSellPrice(priceLine) && !Permission.has(player, Permission.TOWN_SHOP_SELL)) {
+                throw new Exception("&cYou do not have permission.");
+            }
+        } else if (TownyUtils.isNationShop(ownerLine)) {
+            if (PriceUtil.hasBuyPrice(priceLine) && !Permission.has(player, Permission.NATION_SHOP_BUY)) {
+                throw new Exception("&cYou do not have permission.");
+            } else if (PriceUtil.hasSellPrice(priceLine) && !Permission.has(player, Permission.NATION_SHOP_SELL)) {
+                throw new Exception("&cYou do not have permission.");
+            }
+        }
     }
 }
